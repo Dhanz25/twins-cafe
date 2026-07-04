ccbc7c46d35cc8fd73197f551e85ab302231f7f8
Dhanz25 <ramadhanifaiz423@gmail.com>
2026-07-04 11:18:19 +0700

    Fix undefined fake() function in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => $this->faker->name(),
             'level' => 'user',
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
